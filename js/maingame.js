7e7fedabe1faf99be795515ae0ed9d91c2532afd
dlrkd <dlrkd99kh@gmail.com>
2023-05-24 00:03:02 +0900

    update

--- js/maingame.js
+++ js/maingame.js
@@ -114,9 +114,11 @@ function makeCardDeck() {
       // mergedArray 배열을 랜덤하게 섞음
       const shuffledArray = shuffleArray(mergedArray);
 
+      const sortedArray = shuffledArray.slice(0, BOARD_SIZE).sort((a, b) => a - b);
+
     // 섞은 값으로 카드 세팅
     for (let i = 0; i < BOARD_SIZE; i++) {
-        cardDeck.push({card: CARD_IMG[shuffledArray[i]], isOpen: false, isMatch: false});
+        cardDeck.push({card: CARD_IMG[sortedArray[i]], isOpen: false, isMatch: false});
     }
     
     return cardDeck;
